d1080e81c86ba60e8d0f5636b6397deef816bd42
jaredc35 <47196874+jaredc35@users.noreply.github.com>
2019-11-26 12:56:56 -0500

    Update api.py
    Added the GetUpdatedPropertyDetails to api.py in order to pull some additional data from zillow.

--- zillow/api.py
+++ zillow/api.py
@@ -68,6 +68,36 @@ class ValuationApi(object):
 
         return place
 
+    def GetUpdatedPropertyDetails(self, zws_id, zpid):
+        """
+        The GetZestimate API will only surface properties for which a Zestimate exists.
+        If a request is made for a property that has no Zestimate, an error code is returned.
+        Zillow doesn't have Zestimates for all the homes in its database.
+        For such properties, we do have tax assessment data, but that is not provided through the API.
+        For more information, see our Zestimate coverage.
+        :zws_id: The Zillow Web Service Identifier.
+        :param zpid: The address of the property to search. This string should be URL encoded.
+        :param rentzestimate: Return Rent Zestimate information if available (boolean true/false, default: false)
+        :return:
+        """
+
+        url = '%s/GetUpdatedPropertyDetails.htm' % (self.base_url)
+        parameters = {'zws-id': zws_id,
+                      'zpid': zpid}
+
+        resp = self._RequestUrl(url, 'GET', data=parameters)
+        data = resp.content.decode('utf-8')
+
+        xmltodict_data = xmltodict.parse(data)
+        print (xmltodict_data.get('UpdatedPropertyDetails:updatedPropertyDetails',None)['response'])
+        place = Place()
+        # try:
+        #     place.set_data(xmltodict_data.get('UpdatedPropertyDetails:updatedPropertyDetails',None)['response'])
+        # except:
+        #     raise ZillowError({'message': "Zillow did not return a valid response: %s" % data})
+
+        return place
+
     def GetZEstimate(self, zws_id, zpid, rentzestimate=False):
         """
         The GetZestimate API will only surface properties for which a Zestimate exists.
